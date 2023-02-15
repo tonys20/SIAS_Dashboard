48c98b3acaa9b0e3926baf614f5c40501af75637
tonys20 <81080785+tonys20@users.noreply.github.com>
2023-02-15 13:57:56 -0800

    123

--- app.py
+++ app.py
@@ -2,9 +2,20 @@ import streamlit as st
 import yfinance as yf 
 import pandas as pd 
 import os
+import datetime
 
 tickers_df = pd.read_csv('TICKERS.csv')
 sectors_ls = tickers_df.SECTOR.unique()
 tickers_dic = {sectors_ls[i]:list(tickers_df[tickers_df['SECTOR']==sectors_ls[i]]['TICKER']) for i in range(len(sectors_ls))}
+
+@st.cache
+def get_target(sector):
+    today = datetime.date.today()
+    output = pd.DataFrame()
+    for ticker in tickers_dic[sector]:
+        output[f'{ticker} Adj Close'] = yf.download(tickers = ticker, start ='1999-11-30', end =str(today), interval ='1d')['Adj Close']
+    return output
+close_price_df = get_target('INDUSTRIALS')
+
 st.write(tickers_dic)
 st.write(tickers_df)
