3f74ba83b71c51953e4fd5e90125cf3084c6e2bc
tonys20 <81080785+tonys20@users.noreply.github.com>
2023-02-15 13:53:07 -0800

    123

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import yfinance as yf
 import pandas as pd 
 import os
 
-tickers_df = pd.read_csv('TICKERS.csv').reset_index(inplace=True)
+tickers_df = pd.read_csv('TICKERS.csv')
 sectors_ls = tickers_df.SECTOR.unique()
-tickers_dic = {sectors_ls[i]:tickers_df[tickers_df.TICKER]==sectors_ls[i] for i in range(len(sectors_ls))}
+tickers_dic = {sectors_ls[i]:list(tickers_df[tickers_df['SECTOR']==sectors_ls[i]]['TICKER']) for i in range(len(sectors_ls))}
 st.write(tickers_dic)
 st.write(tickers_df)
