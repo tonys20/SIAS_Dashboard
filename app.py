e7b4f4371c6a5c5c56404980a2acb5ce7198de6b
tonys20 <81080785+tonys20@users.noreply.github.com>
2023-02-15 13:37:22 -0800

    123

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ import os
 
 tickers_df = pd.read_csv('TICKERS.csv')
 tickers_ls = tickers_df.SECTOR.unique()
-tickers_dic = {ticker_ls[i]:tickers_df[df[TICKER]==ticker_ls[i]] for i in len(tickers_ls)}
+tickers_dic = {ticker_ls[i]:tickers_df[df[TICKER]==ticker_ls[i]] for i in range(len(tickers_ls))}
 st.write(tickers_dic)
 st.write(tickers_df)
