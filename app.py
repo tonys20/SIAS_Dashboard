c0ca1412411675b19754753b21c749752bedbbbc
tonys20 <81080785+tonys20@users.noreply.github.com>
2023-02-15 15:28:57 -0800

    12

--- app.py
+++ app.py
@@ -16,25 +16,23 @@ def get_target(sector):
     for ticker in tickers_dic[sector]:
         output[f'{ticker} Adj Close'] = yf.download(tickers = ticker, start ='1999-11-30', end =str(today), interval ='1d')['Adj Close']
     return output
+with st.sidebar:
+    sector_selected = st.selectbox('Choose Sector:',sectors_ls)
 
-sector_selected = st.selectbox('Choose Sector:',sectors_ls)
-df = get_target(sector_selected)
 
-start_time =st.date_input(
-    'From:',
-    value = datetime.date(1999, 11, 30),
-    min_value = datetime.date(1999, 11, 30),
-    max_value = datetime.date.today()
-)
+    start_time =st.date_input(
+        'From:',
+        value = datetime.date(1999, 11, 30),
+        min_value = datetime.date(1999, 11, 30),
+        max_value = datetime.date.today()
+    )
 
-end_time = st.date_input(
-    'To:',
-    value = datetime.date(1999, 11, 30),
-    min_value = datetime.date(1999, 11, 30),
-    max_value = datetime.date.today()
-)
-
-custom_df = df.loc[str(start_time): str(end_time)]
+    end_time = st.date_input(
+        'To:',
+        value = datetime.date(1999, 11, 30),
+        min_value = datetime.date(1999, 11, 30),
+        max_value = datetime.date.today()
+    )
 
 def ret_calc(df):
     output = pd.DataFrame()
@@ -43,7 +41,12 @@ def ret_calc(df):
         output['sector_return'] = df.sum(axis = 1)/df.sum(axis = 1).shift(1) - 1
         output['cum_return'] = df.sum(axis = 1)/df.sum(axis = 1)[0] - 1
     return output
+
+df = get_target(sector_selected)
+custom_df = df.loc[str(start_time): str(end_time)]
 ret_df = ret_calc(custom_df)
 st.write(ret_df)
+
+charts_ls = ['cumulative', 'daily']
 fig = px.line(ret_df, y = 'cum_return')
 st.plotly_chart(fig)
