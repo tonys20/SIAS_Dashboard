c09a8d84bf2927340c4c6c1afe7e366ca7686db1
tonys20 <81080785+tonys20@users.noreply.github.com>
2023-02-15 15:56:37 -0800

    12

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ with st.sidebar:
 
     end_time = st.date_input(
         'To:',
-        value = datetime.date(1999, 11, 30),
+        value = datetime.date(2022, 12, 30),
         min_value = datetime.date(1999, 11, 30),
         max_value = datetime.date.today()
     )
@@ -47,6 +47,8 @@ def ret_calc(df):
 df = get_target(sector_selected)
 custom_df = df.loc[str(start_time): str(end_time)]
 ret_df = ret_calc(custom_df)
+
+
 st.write(ret_df)
 
 if chart_type == 'cumulative':
