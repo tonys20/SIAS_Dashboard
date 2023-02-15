5ea76586cc0e22557d925460320ba6b59f70c314
tonys20 <81080785+tonys20@users.noreply.github.com>
2023-02-15 13:31:00 -0800

    12

--- app.py
+++ app.py
@@ -3,15 +3,5 @@ import yfinance as yf
 import pandas as pd 
 import os
 
-def get_tickers():
-    targetdir = os.path.join(os.getcwd(), 'tickers.txt')
-    ticker_dic ={}
-    for filename in filter(lambda p: p.endswith('txt'), os.listdir(targetdir)):
-        filepath =os.path.join(targetdir, filename)
-        with open(filepath, mode='r') as f:
-            lines = f.readlines()
-        ticker_dic[filename.strip('.txt')]=[ticker.strip('\n') for ticker in lines]
-    return ticker_dic
-tickers = get_tickers()
-sectors = list(tickers.keys())
-st.write(sectors)
+tickers = pd.read_csv('TICKERS.csv')
+st.write(tickers)
