c1f39dd2f81d8ee6f4ac12fe50405259bf4df2e7
tonys20 <81080785+tonys20@users.noreply.github.com>
2023-02-15 22:29:34 -0800

    12

--- app.py
+++ app.py
@@ -21,8 +21,9 @@ def get_target(sector):
     today = datetime.date.today()
     output = pd.DataFrame()
     for ticker in tickers_dic[sector]:
-        output[f'{ticker} market_val'] = yf.download(tickers = ticker, start ='1999-11-30', end =str(today), interval ='1d')['Adj Close']
-        output[f'{ticker} market_val'] = output[f'{ticker} Adj Close']*benchmark_df[benchmark_df['TICKER']==ticker]['SHARES']
+        output[f'{ticker} Adj Close'] = yf.download(tickers = ticker, start ='1999-11-30', end =str(today), interval ='1d')['Adj Close']
+        output[f'{ticker}_market_val'] = output[f'{ticker} Adj Close']*benchmark_df[benchmark_df['TICKER']==ticker]['SHARES']
+        output.drop(columns = [f'{ticker} AdjClose'])
     return output
 
 # Macro Indicators from Fred
