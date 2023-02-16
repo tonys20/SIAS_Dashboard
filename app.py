85028525d5dda75efac39fddcb4765f29877894f
tonys20 <81080785+tonys20@users.noreply.github.com>
2023-02-15 22:30:33 -0800

    12

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def get_target(sector):
     for ticker in tickers_dic[sector]:
         output[f'{ticker} Adj Close'] = yf.download(tickers = ticker, start ='1999-11-30', end =str(today), interval ='1d')['Adj Close']
         output[f'{ticker}_market_val'] = output[f'{ticker} Adj Close']*benchmark_df[benchmark_df['TICKER']==ticker]['SHARES']
-        output.drop(columns = [f'{ticker} AdjClose'])
+        output.drop(columns = [f'{ticker} Adj Close'])
     return output
 
 # Macro Indicators from Fred
